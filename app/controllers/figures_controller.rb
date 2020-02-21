7541c0e82d2f7f5177b1fb95e40f9b7387be142d
github username <github email address>
2020-02-20 18:05:38 -0800

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,45 @@
+require "byebug"
+
 class FiguresController < ApplicationController
-  # add controller methods
+  get "/figures/new" do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  post "/figures" do
+    byebug
+    title_info = params[:title]
+    if !title_info[:name].empty?
+      title = Title.create(name: title_info[:name][0])
+    end
+    landmark_info = params[:landmark]
+    if !landmark_info[:name].empty?
+      landmark = Landmark.create(name: landmark_info[:name][0], year_completed: landmark_info[:year_completed][0])
+    end
+    figure = Figure.create(figure[:name])
+    figure.titles << title
+    figure.landmarks << landmark
+    redirect "/figures/#{figure.id}"
+  end
+
+  get "/figures" do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'figures/show'
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'figures/edit'
+  end
+
+  patch "/figures/:id" do
+    Figure.update(params[:id], params[:figure])
+    redirect "/figures/#{params[:id]}"
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,32 @@
+require "byebug"
+
 class LandmarksController < ApplicationController
-  # add controller methods
+  get "/landmarks/new" do
+    erb :'landmarks/new'
+  end
+
+  post "/landmarks" do
+    landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  get "/landmarks" do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :'landmarks/show'
+  end
+
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch "/landmarks/:id" do
+    Landmark.update(params[:id], params[:landmark])
+    redirect "/landmarks/#{params[:id]}"
+  end
 end
